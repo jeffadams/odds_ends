4911218fa11db465fea0cc00b3923b52073c935d
Jeff Adams <jeff.adams@linux.com>
2015-08-25 16:40:55 -0400

    Add JSON check file

---
+++ ack_via_email.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/perl
+# Same thing as parsemail.py, just a little harder to read.
+# Parse an email from stdin and acknowledge a Nagios alert.
+# Passed in from /var/lib/nagios/.procmailrc
+use strict;
+
+my $ack;
+my $command;
+my $commandfile = '/var/lib/nagios3/rw/nagios.cmd';
+my $from;
+my $host;
+my $now = `/bin/date +%s`;
+my $service;
+my $subject;
+
+while (<>) {
+    $from    = $_ if $_ =~ /^From:/ig ;
+    $subject = $_ if $_ =~ /^Subject/ig;
+    $ack     = 1  if $_ =~ /ack/ig;
+}
+
+exit if not $ack;
+
+my $ph; #placeholder
+chomp($from, $now);
+
+if ($subject =~ /Host/) {
+    ($ph, $ph, $host, $ph, $ph, $ph, $ph) = split(/ /, $subject);
+    ($host) = ($host) =~ /(.*)\!/;
+} else {
+    ($ph, $ph, $host, $service, $ph, $ph) = split(/\ /, $subject);
+}   
+
+if ($subject =~ /Host/ ) {
+    $command = "ACKNOWLEDGE_HOST_PROBLEM;$host;1;1;1;email;$from;ack'd by email";
+} else {
+    $command = "ACKNOWLEDGE_SVC_PROBLEM;$host;$service;1;1;1;$from;ack'd by email";
+}
+
+send_command("$command");
+
+sub send_command {
+    my $in=shift;
+    open(F, ">$commandfile") or die "can't open $commandfile";
+    print F "[$now] $in\n";
+    close F;
+}
---
+++ check_json.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# Very basic check for a return of valid JSON. 
+
+import json
+import sys
+import urllib2
+status = { 'OK' : 0 , 'WARNING' : 1, 'CRITICAL' : 2 , 'UNKNOWN' : 3}
+
+errors = "" 
+urllib2.socket.setdefaulttimeout(3)
+
+if len(sys.argv) < 1:
+    sys.stderr.write('Need one argument: check_api.py http://jsonip.com') 
+    sys.exit(1)
+else:
+    URL = sys.argv[1]
+
+try:
+    r = urllib2.urlopen(URL)
+    a = r.read()
+    data = json.loads(a) # This validates. 
+except (urllib2.HTTPError, IOError, ValueError, KeyError, TypeError) as e:
+    errors = ("%s %s" % (URL, e))
+    next
+
+if errors:
+    print("WARN: %s" % errors)
+    sys.exit(status['WARNING'])
+else: 
+    print("OK: %s" % URL)
+    sys.exit(status['OK'])
