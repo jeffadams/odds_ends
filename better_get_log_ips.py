71a19586146e34aec5495dbef4fac35f0c055124
Jeff Adams <jeff.adams@linux.com>
2015-09-08 16:12:28 -0400

    better version

--- better_get_log_ips.py
+++ better_get_log_ips.py
@@ -19,12 +19,8 @@ for line in f:
 
 if len(sys.argv) == 3:
     a = Counter(lines).most_common(int(sys.argv[2]))
-    for x in a:
-        print("%s %s" % (x[1], x[0])) 
 else:
-    a = Counter(lines)
-    # http://docs.python.org/dev/library/collections.html#ordereddict-examples-and-recipes
-    all_ips = OrderedDict(sorted(a.items(), key=lambda t: t[1], reverse=True))
-
-    for x in all_ips:
-        print all_ips[x], x 
+    a = Counter(lines).most_common(len(lines))
+     
+for x in a:
+    print("%s %s" % (x[1], x[0])) 
