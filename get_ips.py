292a7611a8d1d56524ed3278e234f1a4b8b8eed2
Jeff Adams <jeff.adams@linux.com>
2015-09-09 11:34:28 -0400

    use a class

---
+++ get_ips.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+from collections import Counter
+import sys
+
+class GetIps(object):
+    def __init__(self):
+        pass
+    
+    def count(self, file, offset=None): 
+        results = []
+        with open(file) as f:
+            f = f.readlines()
+        for line in f:
+            x = line.split(' ', )
+            results.append(x[0])
+
+        if offset is not None:
+            ip_list = Counter(results).most_common(int(offset))
+    	else:
+            ip_list = Counter(results).most_common(len(results))
+        return ip_list
+
+def main(args):
+    if len(args) < 2:
+        print "Usage: ./get_log_ips.py <logfile> <optional num of ips to display>"
+        sys.exit(0)
+    l = GetIps()
+    if len(args) == 3:
+        a = l.count(args[1], args[2])
+    else:
+        a = l.count(args[1])
+    for x in a:
+        print("%s %s" % (x[1], x[0])) 
+
+if __name__ == "__main__":
+    main(sys.argv)
